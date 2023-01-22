7a02381bf7fc5b90ea304a435e0bbdbdef629fe0
qqyurr <byyulli16@gmail.com>
2023-01-22 22:26:01 +0900

    정다면체 2nd try - qyur

--- Section-2-05_정다면체/정다면체-qyur.js
+++ Section-2-05_정다면체/정다면체-qyur.js
@@ -1,28 +1,16 @@
 const fs = require("fs");
 
-// [1,2,3,4 .. ] 연속되는 숫자를 가진 array를 만드는 함수
-const makeOrderedList = (number) => {
-  let orderedList = [];
-  for (let i = 1; i < Number(number) + 1; i += 1) {
-    orderedList.push(i);
-  }
-  return orderedList;
-}
-
 for (let i = 1; i < 6; i += 1) {
   const [N, M] = fs.readFileSync(`./in${i}.txt`, "utf8").toString().split(' ');
-  let NList = makeOrderedList(N);
-  let MList = makeOrderedList(M);
 
-  // 숫자의 합을 sumNumberCountObject에 저장
   const sumNumberCountObject = {};
-  NList.forEach((NNumber) => {
-    MList.forEach((MNumber) => {
-      const targetKey = String(NNumber + MNumber);
+  for (let n = 1; n < Number(N) + 1; n += 1) {
+    for (let m = 1; m < Number(M) + 1; m += 1) {
+      const targetKey = String(n + m);
       const targetValue = sumNumberCountObject[targetKey];
       sumNumberCountObject[targetKey] = targetValue ? targetValue + 1 : 1;
-    })
-  })
+    }
+  }
 
   const maxCount = Math.max(...Object.values(sumNumberCountObject));
 
